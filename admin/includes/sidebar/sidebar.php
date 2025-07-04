47cf60f6fc0ed27d21048b6af5c9a91a23cedf3f
vickyymosafan <mvickymosafan@gmail.com>
2025-07-04 17:44:36 +0700

    feat: Update sidebar for improved user management and navigation

--- admin/includes/sidebar/sidebar.php
+++ admin/includes/sidebar/sidebar.php
@@ -2,44 +2,55 @@
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin.php">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-keyboard"></i>
+                    <i class="fas fa-user-shield"></i>
                 </div>
-                <div class="sidebar-brand-text mx-3">antosa</div>
+                <div class="sidebar-brand-text mx-3">Admin Antosa</div>
             </a>
 
             <!-- Divider -->
             <hr class="sidebar-divider my-0">
 
             <!-- Nav Item - Dashboard -->
-            <li class="nav-item">
+            <li class="nav-item <?php echo (basename($_SERVER['PHP_SELF']) == 'admin.php') ? 'active' : ''; ?>">
                 <a class="nav-link" href="admin.php">
                     <i class="fas fa-fw fa-tachometer-alt"></i>
-                    <span>Dashboard</span></a>         
+                    <span>Dashboard</span>
+                </a>
             </li>
           
-            
+
             <!-- Divider -->
             <hr class="sidebar-divider">
 
             <!-- Heading -->
             <div class="sidebar-heading">
-                Interface
+                Manajemen User
             </div>
 
-            <!-- Nav Item - Kelola User Proyek -->
+            <!-- Nav Item - Manajemen User (Collapsible) -->
             <li class="nav-item">
-                <a class="nav-link" href="kelola_user_proyek.php">
-                    <i class="fas fa-fw fa-users"></i>
-                    <span>Kelola User Proyek</span></a>
-            </li>
-
-            <!-- Nav Item - Kelola Client -->
-            <li class="nav-item">
-                <a class="nav-link" href="kelola_client.php">
-                    <i class="fas fa-fw fa-user-tie"></i>
-                    <span>Kelola Client</span></a>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser"
+                    aria-expanded="<?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_user_proyek.php', 'kelola_client.php'])) ? 'true' : 'false'; ?>"
+                    aria-controls="collapseUser">
+                    <i class="fas fa-fw fa-users-cog"></i>
+                    <span>Manajemen User</span>
+                </a>
+                <div id="collapseUser" class="collapse <?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_user_proyek.php', 'kelola_client.php'])) ? 'show' : ''; ?>"
+                     aria-labelledby="headingUser" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Kelola User:</h6>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_user_proyek.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_user_proyek.php">
+                            <i class="fas fa-users mr-2"></i> User Proyek
+                        </a>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_client.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_client.php">
+                            <i class="fas fa-user-tie mr-2"></i> Client
+                        </a>
+                    </div>
+                </div>
             </li>
 
             <!-- Divider -->
@@ -50,43 +61,70 @@
                 Manajemen Proyek
             </div>
 
-            <!-- Nav Item - Kelola Tugas Proyek -->
+            <!-- Nav Item - Verifikasi & Approval (Collapsible) -->
             <li class="nav-item">
-                <a class="nav-link" href="kelola_tugas_proyek.php">
-                    <i class="fas fa-fw fa-tasks"></i>
-                    <span>Kelola Tugas Proyek</span></a>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseVerifikasi"
+                    aria-expanded="<?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_tugas_proyek.php', 'kelola_file_gambar.php'])) ? 'true' : 'false'; ?>"
+                    aria-controls="collapseVerifikasi">
+                    <i class="fas fa-fw fa-clipboard-check"></i>
+                    <span>Verifikasi & Approval</span>
+                </a>
+                <div id="collapseVerifikasi" class="collapse <?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_tugas_proyek.php', 'kelola_file_gambar.php'])) ? 'show' : ''; ?>"
+                     aria-labelledby="headingVerifikasi" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Verifikasi:</h6>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_tugas_proyek.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_tugas_proyek.php">
+                            <i class="fas fa-tasks mr-2"></i> Tugas Proyek
+                        </a>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_file_gambar.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_file_gambar.php">
+                            <i class="fas fa-images mr-2"></i> File Gambar
+                        </a>
+                    </div>
+                </div>
             </li>
 
-            <!-- Nav Item - Kelola File Gambar -->
+            <!-- Nav Item - Manajemen RAB & Revisi (Collapsible) -->
             <li class="nav-item">
-                <a class="nav-link" href="kelola_file_gambar.php">
-                    <i class="fas fa-fw fa-images"></i>
-                    <span>Kelola File Gambar</span></a>
-            </li>
-
-            <!-- Nav Item - Kelola RAB Proyek -->
-            <li class="nav-item">
-                <a class="nav-link" href="kelola_rab_proyek.php">
-                    <i class="fas fa-fw fa-file-invoice-dollar"></i>
-                    <span>Kelola RAB Proyek</span></a>
-            </li>
-
-            <!-- Nav Item - Kelola Revisi Request -->
-            <li class="nav-item">
-                <a class="nav-link" href="kelola_revisi_request.php">
-                    <i class="fas fa-fw fa-edit"></i>
-                    <span>Kelola Revisi Request</span></a>
+                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseRAB"
+                    aria-expanded="<?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_rab_proyek.php', 'kelola_revisi_request.php'])) ? 'true' : 'false'; ?>"
+                    aria-controls="collapseRAB">
+                    <i class="fas fa-fw fa-folder-open"></i>
+                    <span>RAB & Revisi</span>
+                </a>
+                <div id="collapseRAB" class="collapse <?php echo (in_array(basename($_SERVER['PHP_SELF']), ['kelola_rab_proyek.php', 'kelola_revisi_request.php'])) ? 'show' : ''; ?>"
+                     aria-labelledby="headingRAB" data-parent="#accordionSidebar">
+                    <div class="bg-white py-2 collapse-inner rounded">
+                        <h6 class="collapse-header">Kelola:</h6>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_rab_proyek.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_rab_proyek.php">
+                            <i class="fas fa-file-invoice-dollar mr-2"></i> RAB Proyek
+                        </a>
+                        <a class="dropdown-item <?php echo (basename($_SERVER['PHP_SELF']) == 'kelola_revisi_request.php') ? 'active bg-primary text-white' : ''; ?>"
+                           href="kelola_revisi_request.php">
+                            <i class="fas fa-edit mr-2"></i> Revisi Request
+                        </a>
+                    </div>
+                </div>
             </li>
 
             <!-- Divider -->
             <hr class="sidebar-divider d-none d-md-block">
 
-             <li class="nav-item">
-                <a class="nav-link" href="../logout.php">
+            <!-- Heading -->
+            <div class="sidebar-heading">
+                Sistem
+            </div>
+
+            <!-- Nav Item - Logout -->
+            <li class="nav-item">
+                <a class="nav-link" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt"></i>
-                    <span>Keluar</span></a>
+                    <span>Keluar</span>
+                </a>
             </li>
-            
+
             <!-- Sidebar Toggler (Sidebar) -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
